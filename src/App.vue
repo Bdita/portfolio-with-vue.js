bd1382bf479e011f6c2321417dc0afc3d31b9cbd
Bandita Sharma <bandita@192-168-1-5.tpgi.com.au>
2017-08-05 18:49:36 +1000

    profile component complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <md-layout md-gutter="16">
+    <md-layout md-flex="30" class='profileContainer'>
+      <router-view> </router-view>
+    </md-layout>
+  </md-layout>
   </div>
 </template>
 
@@ -18,6 +21,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+  margin: 0px;
+  padding: 0px;
+}
+.logo {
+  width: 20%;
+  margin: 2% 0 0 -65% ;
+}
+.profileContainer {
+  margin: 10% 0 0 35%;
 }
 </style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="Profile ">
+    <img src="http://res.cloudinary.com/dihqhbf9i/image/upload/e_art:incognito/v1501820072/11951142_10206755264775458_2690845586774658783_n_xgjsn9.jpg" class='img-circle'>
+    <h3> {{ name.toUpperCase() }} </h3>
+    <hr>
+    <h4 class="profile-title">{{ title.toUpperCase() }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profile',
+  data() {
+    return {
+      title: 'Full-Stack Web Developer',
+      name: 'Bandita Sharma',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Profile {
+  margin: 0 0 0 0%;
+}
+h3 {
+  color: #313640;
+  font-size: 15px;
+}
+h4 {
+  margin-top: 0%;
+  font-weight: 500;
+}
+hr {
+  width: 60px;
+  margin-top: -2%;
+  border: 1.5px solid #41B883;
+}
+.img-circle {
+   border-radius: 50%;
+   width: 40%;
+   box-shadow: 0 0 5px rgba(0, 0, 0, 0.6)
+}
+</style>
